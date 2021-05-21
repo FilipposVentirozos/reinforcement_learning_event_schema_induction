0c668560b68195bc856a717439d9f7e13ea9906e
Philip <filippos.ventirozos@postgrad.manchester.ac.uk>
2021-05-21 11:22:27 +0100

    Updating driver

--- driver.py
+++ driver.py
@@ -27,7 +27,7 @@ class IntervalDriver(driver.Driver, ABC):
             self,
             env: py_environment.PyEnvironment,
             policy: py_policy.PyPolicy,
-            agents_per_recipe: types.Int,
+            # agents_per_recipe: types.Int,
             buffer_observer: Sequence[Callable[[trajectory.Trajectory], Any]],
             observers: Sequence[Callable[[trajectory.Trajectory], Any]],
             transition_observers: Optional[Sequence[Callable[[trajectory.Transition],
@@ -35,18 +35,25 @@ class IntervalDriver(driver.Driver, ABC):
             interval_number_of_recipes: Optional[types.Int] = None):
 
         super(IntervalDriver, self).__init__(env, policy, observers, transition_observers)
-        # A compulsory number of agents per recipe that is pre-defined
-        self._agents_per_recipe = agents_per_recipe
+
+        # Deprecated, A compulsory number of agents per recipe that is pre-defined
+        # self._agents_per_recipe = agents_per_recipe
         # The number of interval
         self._interval_number_of_recipes = interval_number_of_recipes or np.inf
         # Compulsory buffer
         self._buffer_observer = buffer_observer
         # Temporary buffer for each recipe
         self._buffer_template = copy.copy(buffer_observer)  # Do I need to copy
-        self._temp_buffer_reset()
+        self._episode_buffer_reset()
+        self._recipe_buffer_reset()  # Or have a list
+        # Count the recipes to apply reward
+        self.rec_count = 0
 
-    def _temp_buffer_reset(self):
-        self._temp_buffer = self._buffer_template
+    def _episode_buffer_reset(self):
+        self._episode_buffer = self._buffer_template
+
+    def _recipe_buffer_reset(self):
+        self._recipe_buffer = self._buffer_template
 
     def run(self,
             time_step: ts.TimeStep,
@@ -65,23 +72,43 @@ class IntervalDriver(driver.Driver, ABC):
             for observer in self.observers:
                 observer(traj)
             # Add trajectory to buffer
-            self._temp_buffer(traj)
+            self._episode_buffer(traj)
 
-            # Count the agents per recipe
-            num_agents_per_recipe += np.sum(traj.is_boundary())
-            if traj.is_boundary():
-                self.episodic_verb_reward()
-            if num_agents_per_recipe >= self._agents_per_recipe:
-                interval_number_of_recipes += 1
-                num_agents_per_recipe = 0
-                # Calculate the Convolution reward
-                self.convolution_reward()
-                # Pass the Update rewards buffer to the main buffer
-                for traj in self._temp_buffer:
-                    # Can use the add_batch as suggested from comment
-                    self._buffer_observer(traj)
-                # Reet the temporary buffer
-                self._temp_buffer_reset()
+            if traj.is_boundary(): # End of episode
+                # Apply the NE reward estimate
+                # ToDo Get the number of actions
+                trajs = self._episode_buffer.as_dataset(single_deterministic_pass=True)
+                # ToDo Apply the estimate on the Positive rewards
+                self.env.n_NE_estimate
+                log
+                # ToDo Append to recipe buffer
+                self._recipe_buffer.add_batch(trajs)
+                # Re-Init episode buffer
+                self._recipe_buffer_reset()
+
+                # Is it the end of recipe?, should this be done upon greedy policy?
+                if self.rec_count < self.env.rec_count:
+                    self.rec_count = self.env.rec_count
+                    # Apply the Convolutional rewards, could be from list of episode buffers
+                    self._recipe_buffer.as_dataset(single_deterministic_pass=True)
+                    # Calculate convolution reward
+                    # Reset recipe buffer or liste
+            # # Deprecated
+            # # Count the agents per recipe
+            # num_agents_per_recipe += np.sum(traj.is_boundary())
+            # if traj.is_boundary():
+            #     self.episodic_verb_reward()
+            # if num_agents_per_recipe >= self._agents_per_recipe:
+            #     interval_number_of_recipes += 1
+            #     num_agents_per_recipe = 0
+            #     # Calculate the Convolution reward
+            #     self.convolution_reward()
+            #     # Pass the Update rewards buffer to the main buffer
+            #     for traj in self._temp_buffer:
+            #         # Can use the add_batch as suggested from comment
+            #         self._buffer_observer(traj)
+            #     # Reet the temporary buffer
+            #     self._temp_buffer_reset()
 
             time_step = next_time_step
             policy_state = action_step.state
